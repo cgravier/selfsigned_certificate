7df0a5b9c50350a82a8f5e3810825e24d21db0ef
Christophe Gravier <christophe.gravier@telecom-st-etienne.fr>
2013-10-12 16:13:16 +0200

    hotfix: fix version number in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "christophe.gravier@univ-st-etienne.fr"
 license          "All rights reserved"
 description      "Creates/Installs/Configures a selfsigned certificate"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
 %w{ ubuntu debian }.each do |os|
   supports os
