4ed50999117122030db11e65228a65eb2c6f1a88
Christophe Gravier <christophe.gravier@telecom-st-etienne.fr>
2013-10-12 16:01:33 +0200

    splitting cert command for easier debugging for the user

--- Vagrantfile
+++ Vagrantfile
@@ -26,6 +26,7 @@ Vagrant.configure("2") do |config|
   # your network.
 
   # config.vm.network :public_network
+  config.vm.network :forwarded_port, guest: 222, host: 1234
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
--- recipes/default.rb
+++ recipes/default.rb
@@ -13,6 +13,7 @@ package "openssl" do
 end
 
 # create output dir
+log "Creating #{node['selfsigned_certificate']['destination']} (here is stored the self-signed certificate)"
 directory node['selfsigned_certificate']['destination'] do
     owner "root"
     group "root"
@@ -21,16 +22,43 @@ directory node['selfsigned_certificate']['destination'] do
     recursive true
 end
 
+
 # create the certificate: make a request for signature for a certiciate, and have your own CA sign it.
-bash "selfsigned_certificate" do
+log "Running : openssl genrsa -passout pass:#{node[:selfsigned_certificate][:sslpassphrase]} -des3 -out server.key 1024"
+bash "generate certificate" do
   user "root"
   cwd node['selfsigned_certificate']['destination']
   code <<-EOH
         openssl genrsa -passout pass:#{node[:selfsigned_certificate][:sslpassphrase]} -des3 -out server.key 1024
+        EOH
+end
+
+log "Running : openssl req -passin pass:#{node[:selfsigned_certificate][:sslpassphrase]} -subj \"/C=#{node[:selfsigned_certificate][:country]}/ST=#{node[:selfsigned_certificate][:state]}/L=#{node[:selfsigned_certificate][:city]}/O=#{node[:selfsigned_certificate][:orga]}/OU=#{node[:selfsigned_certificate][:depart]}/CN=#{node[:selfsigned_certificate][:cn]}/emailAddress=#{node[:selfsigned_certificate][:email]}\" -new -key server.key -out server.csr"
+bash "generate signature request" do
+  user "root"
+  cwd node['selfsigned_certificate']['destination']
+  code <<-EOH
         openssl req -passin pass:#{node[:selfsigned_certificate][:sslpassphrase]} -subj "/C=#{node[:selfsigned_certificate][:country]}/ST=#{node[:selfsigned_certificate][:state]}/L=#{node[:selfsigned_certificate][:city]}/O=#{node[:selfsigned_certificate][:orga]}/OU=#{node[:selfsigned_certificate][:depart]}/CN=#{node[:selfsigned_certificate][:cn]}/emailAddress=#{node[:selfsigned_certificate][:email]}" -new -key server.key -out server.csr
+        EOH
+end
+
+log "Running: openssl rsa -passin pass:#{node[:selfsigned_certificate][:sslpassphrase]} -in server.key.org -out server.key"
+bash "sign key" do
+  user "root"
+  cwd node['selfsigned_certificate']['destination']
+  code <<-EOH
         cp server.key server.key.org
         openssl rsa -passin pass:#{node[:selfsigned_certificate][:sslpassphrase]} -in server.key.org -out server.key
+        EOH
+end
+
+log "Running: openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt"
+bash "signing the certificate" do
+  user "root"
+  cwd node['selfsigned_certificate']['destination']
+  code <<-EOH
         openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
         EOH
 end
+
 
